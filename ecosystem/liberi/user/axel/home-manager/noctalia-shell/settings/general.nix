08966fc70a8fdd1e5caad872adb8d9390b7c99f2
Axel <axelmychro@gmail.com>
2026-03-10 06:17:43 +0700

    ref: reconfigure radiuses

--- ecosystem/liberi/user/axel/home-manager/noctalia-shell/settings/general.nix
+++ ecosystem/liberi/user/axel/home-manager/noctalia-shell/settings/general.nix
@@ -4,66 +4,65 @@ _: {
     avatarImage = "/home/axel/.face";
     dimmerOpacity = 0.2;
 
+    scaleRatio = 1;
+    radiusRatio = 0.25;
+    iRadiusRatio = 0.25;
+    boxRadiusRatio = 1;
+
     showScreenCorners = false;
     forceBlackScreenCorners = false;
-
-    scaleRatio = 1;
-    radiusRatio = 0.2;
-    iRadiusRatio = 1.0;
-    boxRadiusRatio = 1;
     screenRadiusRatio = 1;
 
-    animationSpeed = -1;
+    animationSpeed = 1;
     animationDisabled = false;
 
-    compactLockScreen = false;
-    lockScreenAnimations = true;
-    lockOnSuspend = true;
+    # compactLockScreen = false;
+    # lockScreenAnimations = true;
+    # lockOnSuspend = true;
 
-    showSessionButtonsOnLockScreen = true;
-    showHibernateOnLockScreen = false;
-    enableLockScreenMediaControls = true;
+    # showSessionButtonsOnLockScreen = true;
+    # showHibernateOnLockScreen = false;
+    # enableLockScreenMediaControls = true;
 
-    enableBlurBehind = false;
-    enableShadows = false;
-    shadowDirection = "bottom_right";
-    shadowOffsetX = 2;
-    shadowOffsetY = 3;
+    # enableBlurBehind = false;
+    # enableShadows = false;
+    # shadowDirection = "bottom_right";
+    # shadowOffsetX = 2;
+    # shadowOffsetY = 3;
 
-    language = "en";
+    # language = "en";
 
-    allowPanelsOnScreenWithoutBar = true;
-    showChangelogOnStartup = true;
+    # allowPanelsOnScreenWithoutBar = true;
+    # showChangelogOnStartup = true;
 
-    enableLockScreenCountdown = true;
-    lockScreenCountdownDuration = 10000;
+    # enableLockScreenCountdown = true;
+    # lockScreenCountdownDuration = 10000;
 
-    autoStartAuth = false;
-    allowPasswordWithFprintd = false;
+    # autoStartAuth = false;
+    # allowPasswordWithFprintd = false;
 
-    clockStyle = "custom";
-    clockFormat = "hh\nmm";
+    # clockStyle = "custom";
+    # clockFormat = "hh\nmm";
 
-    passwordChars = false;
+    # passwordChars = false;
 
-    lockScreenMonitors = [ ];
+    # lockScreenMonitors = [ ];
 
-    lockScreenBlur = 0.0;
-    lockScreenTint = 0.0;
+    # lockScreenBlur = 0.0;
+    # lockScreenTint = 0.0;
 
-    keybinds = {
-      keyUp = [ "Up" ];
-      keyDown = [ "Down" ];
-      keyLeft = [ "Left" ];
-      keyRight = [ "Right" ];
-      keyEnter = [
-        "Return"
-        "Enter"
-      ];
-      keyEscape = [ "Esc" ];
-      keyRemove = [ "Del" ];
-    };
-    reverseScroll = false;
+    # keybinds = {
+    #   keyUp = [ "Up" ];
+    #   keyDown = [ "Down" ];
+    #   keyLeft = [ "Left" ];
+    #   keyRight = [ "Right" ];
+    #   keyEnter = [
+    #     "Return"
+    #     "Enter"
+    #   ];
+    #   keyEscape = [ "Esc" ];
+    #   keyRemove = [ "Del" ];
+    # };
+    # reverseScroll = false;
   };
-
 }
