6dfe2d1e2b9353692adafcd32460f9421808cb83
Axel <axelmychro@gmail.com>
2026-03-13 20:36:11 +0700

    ref: reconfigure ghostty

--- ecosystem/liberi/user/axel/home-manager/programs/ghostty.nix
+++ ecosystem/liberi/user/axel/home-manager/programs/ghostty.nix
@@ -4,62 +4,32 @@ _: {
     enableBashIntegration = false;
 
     themes = {
-      catppuccin-frappe = {
-        background = "303446";
-        foreground = "c6d0f5";
-        cursor-color = "f2d5cf";
-        cursor-text = "232634";
-        palette = [
-          "0=#51576d"
-          "1=#e78284"
-          "2=#a6d189"
-          "3=#e5c890"
-          "4=#8caaee"
-          "5=#f4b8e4"
-          "6=#81c8be"
-          "7=#a5adce"
-          "8=#626880"
-          "9=#e78284"
-          "10=#a6d189"
-          "11=#e5c890"
-          "12=#8caaee"
-          "13=#f4b8e4"
-          "14=#81c8be"
-          "15=#b5bfe2"
-        ];
-        selection-background = "44495d";
-        selection-foreground = "c6d0f5";
-        split-divider-color = "414559";
-      };
       nord = {
-        background = "2e3440";
+        background = "3b4252";
         foreground = "d8dee9";
-
-        cursor-color = "88c0d0";
-        cursor-text = "2e3440";
-
+        cursor-color = "e5e9f0";
+        cursor-text = "eceff4";
         palette = [
-          "0=#3b4252"
+          "0=#434c5e"
           "1=#bf616a"
           "2=#a3be8c"
           "3=#ebcb8b"
-          "4=#81a1c1"
+          "4=#5e81ac"
           "5=#b48ead"
-          "6=#88c0d0"
-          "7=#e5e9f0"
+          "6=#8fbcbb"
+          "7=#81a1c1"
           "8=#4c566a"
           "9=#bf616a"
           "10=#a3be8c"
           "11=#ebcb8b"
-          "12=#81a1c1"
+          "12=#5e81ac"
           "13=#b48ead"
           "14=#8fbcbb"
-          "15=#eceff4"
+          "15=#b5bfe2"
         ];
-
-        selection-background = "434c5e";
-        selection-foreground = "eceff4";
-        split-divider-color = "4c566a";
+        selection-background = "81a1c1";
+        selection-foreground = "d8dee9";
+        split-divider-color = "81a1c1";
       };
     };
     settings = {
@@ -73,7 +43,7 @@ _: {
       mouse-hide-while-typing = true;
       mouse-scroll-multiplier = 3;
 
-      title = "PRTS";
+      title = "catastrophe";
       window-decoration = "none";
       window-height = 24;
       window-width = 80;
