c9baf17a401c324661b8347c6b3f9b7a2cdbb289
Axel <axelmychro@gmail.com>
2026-03-14 20:03:35 +0700

    ref: increase verbosity

--- ecosystem/skadi/user/axel/home-manager/shell/fish/functions.nix
+++ ecosystem/skadi/user/axel/home-manager/shell/fish/functions.nix
@@ -5,22 +5,22 @@ _: {
         argparse r/reboot u/update w/wipe -- $argv
         or return
 
-        if set -q _flag_update
-          nix flake update
-        end
         if set -q _flag_wipe
-            nix profile wipe-history
-            nix store gc
-            nix store optimise
+            nix profile wipe-history --verbpse
+            nix store gc --verbose
+            nix store optimise --verbose
+        end
+        if set -q _flag_update
+          nix flake update --verbose
         end
 
         fd -e nix -x nixfmt {}
         fd -e kdl -x kdlfmt format {}
         git add .
 
-        if sudo nixos-rebuild switch --flake .#skadi
+        if sudo nixos-rebuild switch --verbose --flake .#skadi
           if set -q _flag_reboot
-            echo "Rebooting in 5 seconds... (Ctrl+C to cancel)"
+            echo "rebooting in 5 seconds. (Ctrl+C to cancel)"
             sleep 5
             systemctl reboot
           end
