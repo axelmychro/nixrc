356cf4add4460ac3611dbb278e1b701ca499cf82
Axel <axelmychro@gmail.com>
2026-03-15 16:32:13 +0700

    fix: wrong block

--- ecosystem/priestess/user/axel/home-manager/shell/bash/index.nix
+++ ecosystem/priestess/user/axel/home-manager/shell/bash/index.nix
@@ -1,7 +1,7 @@
 _: {
+  home.shell.enableBashIntegration = true;
   programs.bash = {
     enable = true;
     initExtra = builtins.readFile ./init.sh;
   };
-  shell.enableBashIntegration = true;
 }
