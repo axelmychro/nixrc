6b1834b047488d610a4f68416e2161986d3e733b
Axel <axelmychro@gmail.com>
2026-03-17 13:02:10 +0700

    ref: more git abbrs

--- ecosystem/skadi/user/axel/home-manager/shell/fish/abbrs/git.nix
+++ ecosystem/skadi/user/axel/home-manager/shell/fish/abbrs/git.nix
@@ -1,39 +1,32 @@
 _: {
   programs.fish.shellAbbrs = {
     g = "git";
-
-    gs = "git status";
-
-    gd = "git diff";
-    gds = "git diff --staged";
-
-    gl = "git log";
-    gll = "git log --oneline --graph --decorate --all";
+    gg = "git status";
+    ggg = "git status; and git fetch; and git stash list";
 
     ga = "git add";
     gaa = "git add .";
 
-    gc = "git commit -m";
-    gca = "git commit --amend";
+    gb = "git branch";
+    gbd = "git branch -d";
 
     gco = "git checkout";
     gcob = "git checkout -b";
 
-    gb = "git branch";
-    gbd = "git branch -d";
-
-    gsh = "git stash";
-    gsha = "git stash apply";
-    gshp = "git stash pop";
-    gshl = "git stash list";
-
     gcn = "git clean";
     gcnn = "git clean -fd";
 
-    gr = "git rebase";
-    gri = "git rebase -i";
+    gct = "git commit -m";
+    gcf = "git commit --fixup";
+    gctt = "git commit --amend --no-edit";
+
+    gd = "git diff";
+    gds = "git diff --staged";
 
     gf = "git fetch";
+
+    gl = "git log";
+    gll = "git log --oneline --graph --decorate --all";
 
     gpl = "git pull";
     gplr = "git pull --rebase";
@@ -42,6 +35,20 @@ _: {
     gphf = "git push --force";
     gphl = "git push --force-with-lease";
 
-    grh = "git reset --hard";
+    grb = "git rebase";
+    grbi = "git rebase -i";
+
+    grs = "git reset";
+    grsh = "git reset --hard";
+
+    grm = "git remote";
+
+    gs = "git stash";
+    gsu = "git stash -u";
+    gsm = "git stash push -m";
+    gsum = "git stash push -um";
+    gsa = "git stash apply";
+    gsd = "git stash drop";
+    gsp = "git stash pop";
   };
 }
