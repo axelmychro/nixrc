c68067f90f895375f410d35b3229fca794a927e8
Axel <axelmychro@gmail.com>
2026-03-17 11:39:00 +0700

    feat: more quick search

--- user/axel/home-manager/programs/zen-browser/search.nix
+++ user/axel/home-manager/programs/zen-browser/search.nix
@@ -1,17 +1,27 @@
-{
-  pkgs,
-  ...
-}:
-{
+_: {
   programs.zen-browser.profiles.default.search = {
     force = true;
     default = "ddg";
     engines = {
+      pkgs = {
+        name = "NixOS Packages";
+        urls = [ { template = "https://search.nixos.org/packages?query={searchTerms}"; } ];
+        definedAliases = [ "@np" ];
+      };
+      options = {
+        name = "NixOS Options";
+        urls = [ { template = "https://search.nixos.org/options?query={searchTerms}"; } ];
+        definedAliases = [ "@no" ];
+      };
+      wiki = {
+        name = "NixOS Options";
+        urls = [ { template = "https://wiki.nixos.org/w/index.php?search={searchTerms}"; } ];
+        definedAliases = [ "@nw" ];
+      };
       mynixos = {
         name = "MyNixOS";
         urls = [ { template = "https://mynixos.com/search?q={searchTerms}"; } ];
-        icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-        definedAliases = [ "@mno" ];
+        definedAliases = [ "@mn" ];
       };
     };
   };
