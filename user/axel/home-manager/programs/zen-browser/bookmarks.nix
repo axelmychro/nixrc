aee73f75b47c187439fad927d2b6408cb029827e
Axel <axelmychro@gmail.com>
2026-03-24 11:21:59 +0700

    chore: disable first space

--- user/axel/home-manager/programs/zen-browser/bookmarks.nix
+++ user/axel/home-manager/programs/zen-browser/bookmarks.nix
@@ -2,20 +2,20 @@ _: {
   programs.zen-browser.profiles.default.bookmarks = {
     force = true;
     settings = [
-      {
-        name = "Axel";
-        toolbar = true;
-        bookmarks = [
-          {
-            name = "Whatsapp";
-            url = "https://web.whatsapp.com";
-          }
-          {
-            name = "GitHub";
-            url = "https://github.com/axelmychro";
-          }
-        ];
-      }
+      # {
+      #   name = "Axel";
+      #   toolbar = true;
+      #   bookmarks = [
+      #     {
+      #       name = "Whatsapp";
+      #       url = "https://web.whatsapp.com";
+      #     }
+      #     {
+      #       name = "GitHub";
+      #       url = "https://github.com/axelmychro";
+      #     }
+      #   ];
+      # }
     ];
   };
 }
