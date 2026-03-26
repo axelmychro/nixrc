0b214c78c0dcc3846f27abcb3c8a071731802615
Axel <axelmychro@gmail.com>
2026-03-26 17:46:47 +0700

    ref: update default browser

--- user/axel/home-manager/default-apps/internet.nix
+++ user/axel/home-manager/default-apps/internet.nix
@@ -1,6 +1,6 @@
 _: {
   xdg.mimeApps.defaultApplications = {
-    "x-scheme-handler/http" = [ "zen-twilight.desktop" ];
-    "x-scheme-handler/https" = [ "zen-twilight.desktop" ];
+    "x-scheme-handler/http" = [ "firefox.desktop" ];
+    "x-scheme-handler/https" = [ "firefox.desktop" ];
   };
 }
