98492bd6e8f7a52d1382eb42317aac5eb3651dea
Axel <axelmychro@gmail.com>
2026-09-07 06:52:39 +0700

    Common (desktop) utilities: Enable packages

--- common/desktop/utilities.nix
+++ common/desktop/utilities.nix
@@ -7,6 +7,8 @@
 {
   config = lib.mkIf config.common.utilities.enable {
     environment.systemPackages = with pkgs; [
+      bitwarden-desktop
+      sherlock
       cpu-x
       gparted
       gpu-viewer
